520420a6ee717caf2b615e38930e472729433c61
Alekkivi <Aleksi.Kivilehto@metropolia.fi>
2024-01-18 10:32:33 +0200

    added express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-// main JS file
+// Main JS file
+import express from 'express';
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
 
-console.log('nodemon toimiii');
+app.get('/items', (req, res) => {
+  res.json([
+    {id: 1, name: 'Item1'},
+    {id: 2, name: 'Item2'},
+  ]);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST api!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
